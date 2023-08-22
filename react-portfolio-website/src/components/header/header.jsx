cdf5926d6edb5e84837035092c1de7ee4d96f611
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-22 22:05:08 +0100

    added call to action to header

--- react-portfolio-website/src/components/header/header.jsx
+++ react-portfolio-website/src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
@@ -8,7 +9,7 @@ const header = () => {
         <h5> Hello I'm</h5>
         <h1>Tomislav Dinev</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
-        
+        <CTA />
       </div>
     </header>
   )
