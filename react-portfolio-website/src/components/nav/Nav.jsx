6f6ae54a4cebfc24a2f4388c2acc31ce39092bd5
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-23 21:55:38 +0100

    beginning of nav

--- react-portfolio-website/src/components/nav/Nav.jsx
+++ react-portfolio-website/src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {ImHome} from 'react-icons/im'
+import {BiSolidUserDetail} from 'react-icons/bi'
+import {FaBook} from 'react-icons/fa'
+import {RiCustomerServiceFill} from 'react-icons/ri'
+import {BiSolidMessageDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><ImHome /></a>
+      <a href='#about'><BiSolidUserDetail /></a>
+      <a href='#experience'><FaBook /></a>
+      <a href='#services'><RiCustomerServiceFill /></a>
+      <a href='#contact'><BiSolidMessageDetail /></a>
+    </nav>
   )
 }
 
