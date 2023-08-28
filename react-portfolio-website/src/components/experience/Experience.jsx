4050d53ac4d70ad354de42640caedac3dc241e23
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-28 15:48:25 +0100

    fixes on experience

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -14,28 +14,33 @@ const Experience = () => {
         <div className='experience__content'>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>HTML</h4>
+        <div><h4>HTML</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>CSS</h4>
+        <div><h4>CSS</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>JavaScript</h4>
+        <div><h4>JavaScript</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>Bootstrap</h4>
+        <div><h4>Bootstrap</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>React</h4>
+        <div><h4>React</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         </div>
         </div>
@@ -44,28 +49,33 @@ const Experience = () => {
         <div className='experience__content'>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>Node JS</h4>
+        <div><h4>Node JS</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>MongoDB</h4>
+        <div><h4>MongoDB</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>Python</h4>
+        <div><h4>Python</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>PostgreSQL</h4>
+        <div><h4>PostgreSQL</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         <article className='experience__details'>
         <BsCheckCircleFill />
-        <h4>Flask</h4>
+        <div><h4>Flask</h4>
         <small className='text-light'>Experienced</small>
+        </div>
         </article>
         </div> 
         </div>
