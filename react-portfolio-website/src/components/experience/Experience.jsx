4b2b024f50775ef7b81d9215221b024a64547ee9
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-28 15:14:45 +0100

    frontend section for experience

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {BsCheckCircleFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -9,10 +10,64 @@ const Experience = () => {
 
       <div className='container experience__container'>
         <div className='experience__frontend'>
-
+        <h3>Frontend Development</h3>
+        <div className='experience__content'>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>HTML</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>CSS</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>JavaScript</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>Bootstrap</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>React</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        </div>
         </div>
         <div className='experinece__backend'>
-          
+        <h3>Frontend Development</h3>
+        <div className='experience__content'>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>HTML</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>CSS</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>JavaScript</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>Bootstrap</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        <article className='experience__details'>
+        <BsCheckCircleFill />
+        <h4>React</h4>
+        <small className='text-light'>Experienced</small>
+        </article>
+        </div>
         </div>
       </div>
     </section>
