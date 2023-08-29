db4aaf5614c6f34909020bcf7be52c7f328ab7b7
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-29 16:47:41 +0100

    data

--- react-portfolio-website/src/components/testimonials/Testimonials.jsx
+++ react-portfolio-website/src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,37 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+
+const data = [
+  {
+  avatar: Avtı,
+  name: 'Tina Snow',
+  review: `Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro
+  blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores
+  deserunt explicabo nostrum ducimus quasi?`
+  },
+  {
+  avatar: Avt2,
+  name: 'Shatta Wale',
+  review: `Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro
+  blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores
+  deserunt explicabo nostrum ducimus quasi?`
+  },
+  {
+  avatar: Avt3,
+  name: 'Kwame Despite',
+  review: `Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro
+  blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores
+  deserunt explicabo nostrum ducimus quasi?`
+  },
+  {
+  avatar: Avt4,
+  name: 'Nana Ama McBrown',
+  review: `Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro
+  blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores
+  deserunt explicabo nostrum ducimus quasi?`
+  },
+]
 const Testimonials = () => {
   return (
   <section id='testimonials'>
