cb6d95d7426dc394bac35662c4aec526a8cb720b
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-29 15:46:40 +0100

    start of portfolio part

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com/dinevtomislav' className='btn'>Github</a>
+          <a href='https://github.com/dinevtomislav' className='btn-primary'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
