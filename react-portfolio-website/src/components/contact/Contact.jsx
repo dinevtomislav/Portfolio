b519753f081b14be2827befc1bbdf627b3667c8c
Tomislav Dinev <dinevtomislav@gmail.com>
2023-08-30 15:17:22 +0100

    beginning of contact section

--- react-portfolio-website/src/components/contact/Contact.jsx
+++ react-portfolio-website/src/components/contact/Contact.jsx
@@ -3,7 +3,21 @@ import './contact.css'
 
 const Contacts = () => {
   return (
-    <section id='contact'>Contacts</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <h4>Email</h4>
+            <h5>dinevtomislav@gmail.com</h5>
+            <a href='mailto:dinevtomislav@gmail.com'>Send A Message!</a>
+          </article> 
+        </div>
+        <form action=''></form>
+      </div>
+    </section>
   )
 }
 
