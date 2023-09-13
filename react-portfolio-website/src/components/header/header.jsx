883d2312be255f7a4c0287b235e69b09548aa1ef
Tomislav Dinev <dinevtomislav@gmail.com>
2023-09-13 16:15:40 +0100

    adding animations

--- react-portfolio-website/src/components/header/header.jsx
+++ react-portfolio-website/src/components/header/header.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/headshot.png'
 import HeaderSocials from './HeaderSocials'
 
-const header = () => {
+const Header = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadImage = () => {
+      setImageLoaded(true);
+    };
+
+    const image = new Image();
+    image.src = ME;
+    image.onload = loadImage;
+  }, []);
+
   return (
     <header>
       <div className='container header__container'>
-        <h5> Hello I'm</h5>
-        <h1>Tomislav Dinev</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='bubbly'> Hello I'm</h5>
+        <h1 className='bubbly'>Tomislav Dinev</h1>
+        <h5 className='bubbly'>Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
 
-        <div className='me'>
+        <div className={`me ${imageLoaded ? 'me-loaded' : ''}`}>
           <img src={ME} alt="" />
         </div>
 
@@ -24,4 +36,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
