3e92d0acb65a38567bf6e255aeed9dbdd6778f4f
Tomislav Dinev <dinevtomislav@gmail.com>
2023-10-23 17:10:28 +0100

    improvement to about info

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -40,10 +40,14 @@ const About = () => {
           </div>
 
           <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut doloremque
-quaerat perferendis consectetur consequuntur impedit eum magni
-dolorem perspiciatis repellendus quisquam reprehenderit, error
-voluptatem necessitatibus. Hic minus cum soluta iure?
+          I started this great journey back in high school with the start of GCSEs as I decided to study Computer Science.
+This interest continued in my later years as I went on to study IT in college where I loved learning new things and
+improving my skills. As I kept on improving, I enjoyed practicing what I have learned so that it can stick with me
+and the best way I did this was by helping my peers to get a breakthrough in their assignments as it was a tough
+time with Covid as the main culprit. I have been always interested in technologies and the betterment they
+provide for everybody and everything as efficiency is something I very much enjoy. While I do love to improve my
+technical skills, I still try to improve myself in other ways by working out on a weekly consistent basis and
+spending time with family and friends.
           </p>
 
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
