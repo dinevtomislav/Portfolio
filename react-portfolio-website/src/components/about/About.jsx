8117b428673fa82fd81f8809bc5d23a66c5f528e
Tomislav Dinev <dinevtomislav@gmail.com>
2023-10-24 16:38:22 +0100

    fixed

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             <article className='about__card'>
             <MdFolderCopy className='about__icon'/>
               <h5>Projects</h5>
-              <small>5+ Completed Projects</small>
+              <small>4+ Completed Projects</small>
             </article>
           </div>
 
